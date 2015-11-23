c2e9bf6cec3761b6a5dc52f3df8752a2a9e79ee0
deepod <davidapitt@gmail.com>
2015-11-23 23:13:37 +0100

    yop yo yo yo

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,8 @@
 class HelloWorld{
 	public static void main(String[] args){
 		System.out.println("Hello World");
-		System.out.println("How are you?");
-
+		System.out.println("yo yo yo yo yo yo yo yo yo");
+		System.out.println("Bla bla bla bla bla bla bla");
 		
 	}
 }
